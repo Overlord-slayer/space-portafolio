d06fb3099750a8ce13b868506738a09fde608a7d
Samuel Argueta <jarguetahernandez02@gmail.com>
2024-02-22 12:33:16 -0600

    Se agrego el CV modificado

--- components/main/About.tsx
+++ components/main/About.tsx
@@ -63,18 +63,18 @@ export default function About() {
                 repeat={Infinity}
               />
             </h1>
-            <div className='gap-3 mt-[50px]'>
-              <h1 className='text-2xl md:text-4xl lg:text-5xl xl:text-6xl'>
-                <span className='font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-2 md:py-4 xl:py-6'>
-                  Desarrollador web Junior
-                </span>
-              </h1>
-              <p className='text-sm md:text-base xl:text-lg text-center mt-4 gap-3'>
-                Actualmente estudiante, cursando 4to año de la carrera Ciencias de la Computación.   
-                <br />
-                Posee interés por la inteligencia artificial, así como por la ciberseguridad.
-              </p>
-            </div>
+            
+            <h1 className='mt-[70px] text-2xl md:text-4xl lg:text-5xl xl:text-6xl'>
+              <span className='font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-2 md:py-4 xl:py-6'>
+                Desarrollador web Junior
+              </span>
+            </h1>
+            <p className='text-sm md:text-base xl:text-lg text-center mt-4 gap-3'>
+              Actualmente estudiante, cursando 4to año de la carrera Ciencias de la Computación.   
+              <br />
+              Posee interés por la inteligencia artificial, así como por la ciberseguridad.
+            </p>
+            
             
           </motion.div>
         </motion.div>
